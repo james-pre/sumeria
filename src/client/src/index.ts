5a2cf3302ee8f83158b3020520f5a62fd0c94373
James Prevett <jp@jamespre.dev>
2026-09-17 12:04:39 -0500

    Added client exports

--- src/client/src/index.ts
+++ src/client/src/index.ts
@@ -1,1 +1,4 @@
 export * as controls from './controls.js';
+export * as input from './input.js';
+export * as render from './render_thread.js';
+export * as socket from './socket.js';
--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -2,6 +2,6 @@ import * as render from './render_thread.js';
 
 const _port = Number(new URLSearchParams(location.search).get('port'));
 
-export const serverPort = Number.isSafeInteger(_port) ? _port : null;
+export const serverPort = Number.isSafeInteger(_port) && _port > 0 ? _port : null;
 
 render.init();
